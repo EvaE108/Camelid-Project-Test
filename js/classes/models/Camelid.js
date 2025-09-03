df21b65a2d42148df99a0e8bf7e3ad2a96fea632
EvaE108 <144815601+EvaE108@users.noreply.github.com>
2025-09-03 15:31:26 -0400

    Update Camelid.js

--- js/classes/models/Camelid.js
+++ js/classes/models/Camelid.js
@@ -5,7 +5,7 @@ import { Model } from "./models.js";
 
 export default new Model(
   "Camelid",            // <- name shown in-app and used for model folder
-  bonesList,            // same list as Canine
+  [],            // same list as Canine
   0.032,                // same scale
   [0, 8, 0],            // same center
   "Camelid Model",      // label under the button
