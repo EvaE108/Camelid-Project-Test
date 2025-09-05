b13e2d7db44e4b042005a9466460af0840f353f0
EvaE108 <144815601+EvaE108@users.noreply.github.com>
2025-09-05 15:04:13 -0400

    Update Camelid.js

--- js/classes/models/Camelid.js
+++ js/classes/models/Camelid.js
@@ -1,8 +1,6 @@
 import {Model} from "./models.js";
 
-let bonesList = ['potato',
-                'sitarbuckss',
-                ];
+let bonesList = ['potato'];
 
 export default new Model(
     "Camelid",
